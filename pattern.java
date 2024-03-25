69771a63a8f25de076982ddaafa23b674c4d0bb7
vedantijadhav18 <jadhavvedanti87@gmail.com>
2024-03-25 14:06:45 +0530

    welcome page

---
+++ pattern.java
@@ -0,0 +1,25 @@
+public class pattern {
+    
+    public static void printPattern(int n)
+    {
+        int i, j;
+        
+        for (i = 1; i <= n; i++) {
+            
+            for (j = 1; j <= i; j++) {
+                
+                System.out.print(j + " ");
+            }
+ 
+           
+            System.out.println();
+        }
+    }
+ 
+   
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}
